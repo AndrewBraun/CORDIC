d14bc17cd19f2f0a6443c1141bcd29cc5951e40e
Andrew <ABandrewbraun@gmail.com>
2019-07-19 09:28:55 -0700

    Slight changes to cordic_main.c

--- cordic_main.c
+++ cordic_main.c
@@ -7,6 +7,33 @@ int z_table[15] = { 25735, 15192, 8027, 4074, 2045,
 	1023, 511, 255, 127, 63, 31, 15, 7, 3, 1};
 
 void verify_V( int x_i_init, int y_i_init, int z_i_init,
+			int x_i, int y_i, int z_i);
+
+void verify_R( int x_i_init, int y_i_init, int z_i_init,
+			int x_i, int y_i, int z_i);
+
+typedef enum {V, R} Mode;
+
+int main( void) {
+	int x_i_init, y_i_init, z_i_init;
+	int x_i, y_i, z_i;
+	Mode curMode = V;
+
+	x_i = ( x_i_init = 27852);
+	y_i = ( y_i_init = 24903);
+	z_i_init = 0;
+
+	if ( curMode == V) {
+		cordic_V_fixed_point( &x_i, &y_i, &z_i);
+		verify_V( x_i_init, y_i_init, z_i_init, x_i, y_i, z_i);
+	}
+	else {
+		cordic_R_fixed_point( &x_i, &y_i, &z_i);
+		verify_R( x_i_init, y_i_init, z_i_init, x_i, y_i, z_i);
+	}
+}
+
+void verify_V( int x_i_init, int y_i_init, int z_i_init,
 			int x_i, int y_i, int z_i){
 	
 	double x_d_init, y_d_init, z_d_init, x_d, y_d, z_d;
@@ -52,27 +79,6 @@ void verify_R( int x_i_init, int y_i_init, int z_i_init,
 	printf("x_i = %5i\t\tx_d = %f\n", x_i, x_d);
 	printf("y_i = %5i\t\ty_d = %f\n", y_i, y_d);
 	printf("z_i = %5i\t\tz_d = %f\n", z_i, z_d);
-}
-
-typedef enum {V, R} Mode;
-
-int main( void) {
-	int x_i_init, y_i_init, z_i_init;
-	int x_i, y_i, z_i;
-	Mode curMode = V;
-
-	x_i = ( x_i_init = 27852);
-	y_i = ( y_i_init = 24903);
-	z_i_init = 0;
-
-	if ( curMode == V) {
-		cordic_V_fixed_point( &x_i, &y_i, &z_i);
-		verify_V( x_i_init, y_i_init, z_i_init, x_i, y_i, z_i);
-	}
-	else {
-		cordic_R_fixed_point( &x_i, &y_i, &z_i);
-		verify_R( x_i_init, y_i_init, z_i_init, x_i, y_i, z_i);
-	}
 }
 
 
