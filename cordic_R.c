88e66ff5dd26aaa834ad0e1968671f40f12ca61b
Andrew <ABandrewbraun@gmail.com>
2019-07-19 19:25:10 -0700

    Minor changes for optimization

--- cordic_R.c
+++ cordic_R.c
@@ -5,7 +5,7 @@ int z_table[15];
 void cordic_R_fixed_point( int *x, int *y, int *z) {
 	int x_temp_1, y_temp_1, z_temp;
 	int x_temp_2, y_temp_2;
-	int i;
+	register int i;
 
 	x_temp_1 = *x;
 	y_temp_1 = *y;
