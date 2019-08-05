6e21932ace14aef171f6aaf7a0002e669567c06f
Andrew <ABandrewbraun@gmail.com>
2019-08-05 15:48:35 -0700

    Minor reordering to cordic_R.c for optimization

--- cordic_R.c
+++ cordic_R.c
@@ -16,30 +16,30 @@ void cordic_R_fixed_point( int * restrict x, int * restrict y, int * restrict z)
 
 		if ( z_temp < 0) { // direction = -1
 
+			z_temp += z_table[i];
 			x_temp_2 = x_temp_1 + (y_temp_1 >> i);
 			y_temp_2 = y_temp_1 - (x_temp_1 >> i);
-			z_temp += z_table[i];
 		}
 		else { // direction = 1
 
+			z_temp -= z_table[i];
 			x_temp_2 = x_temp_1 - (y_temp_1 >> i);
 			y_temp_2 = y_temp_1 + (x_temp_1 >> i);
-			z_temp -= z_table[i];
 		}
 
 		i++;
 
 		if ( z_temp < 0) { // direction = -1
 
+			z_temp += z_table[i];
 			x_temp_1 = x_temp_2 + (y_temp_2 >> i);
 			y_temp_1 = y_temp_2 - (x_temp_2 >> i);
-			z_temp += z_table[i];
 		}
 		else { // direction = 1
 
+			z_temp -= z_table[i];
 			x_temp_1 = x_temp_2 - (y_temp_2 >> i);
 			y_temp_1 = y_temp_2 + (x_temp_2 >> i);
-			z_temp -= z_table[i];
 		}
 
 		//x_temp_1 = x_temp_2;
